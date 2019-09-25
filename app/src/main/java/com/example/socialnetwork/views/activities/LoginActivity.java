045aaa023175585e275addc125f6c7c053471b02
Anil Kumar <anilkumar7717@gmail.com>
2019-09-25 23:33:14 +0530

    firebase account changed
    firebase account changed

--- app/src/main/java/com/example/socialnetwork/views/activities/LoginActivity.java
+++ app/src/main/java/com/example/socialnetwork/views/activities/LoginActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -13,9 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.socialnetwork.R;
 import com.google.android.gms.auth.api.Auth;
